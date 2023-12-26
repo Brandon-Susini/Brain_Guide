12a46848bf485bb952ebcb9409ce52581c495a6f
Brandon Susini <brandonsusini@gmail.com>
2023-12-26 11:27:39 -0500

    Partial Fix to Original Polygon mouse detection

--- scenes/brain_orig.gd
+++ scenes/brain_orig.gd
@@ -1,9 +1,23 @@
 extends CanvasGroup
 
+@onready var brain_offset: Vector2 = position
+@onready var collision_shape = $O2.polygon
+
+func _ready():
+	
+	for i in range(len(collision_shape)):
+		print("Before -> ", collision_shape[i])
+		collision_shape.set(i,collision_shape[i] + ($O2.position)) #+ brain_offset))
+		print("After -> ", collision_shape[i])
+	print("Brain Offset:")
+	print(brain_offset)
+	print("Collision Shape:")
+	print(collision_shape)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Geometry2D.is_point_in_polygon(get_global_mouse_position(),$O2.polygon):
+	if Geometry2D.is_point_in_polygon(get_global_mouse_position(),collision_shape):
 		$O2.color = Color.PURPLE
 		print("Something")
 	else:
